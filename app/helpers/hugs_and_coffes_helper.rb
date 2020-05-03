da7cd1da6c64beef04dfb2ce5c045a8d700de664
xXangelXx <angeluchimag17@gmail.com>
2020-05-03 06:24:40 -0400

    view hugsAndCoffe Home

---
+++ app/helpers/hugs_and_coffes_helper.rb
@@ -0,0 +1,2 @@
+module HugsAndCoffesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :estado, :operation, :perfil
+get 'hugsAndCoffes', to: 'hugs_and_coffes#new'
+#  resources :hugs_and_coffes, only: [:new],  as: :hugsAndCoffes
+resources :estado, :operation
+resources :perfils, except: [:index]
 resources :thanks, only: [:create]
 end
