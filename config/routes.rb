a4d786800f2c9741023e370394a3daf1e1fc278c
xXangelXx <angeluchimag17@gmail.com>
2020-05-03 09:20:38 -0400

    modified new.html.erb hugs_and_coffes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get 'hugsAndCoffes', to: 'hugs_and_coffes#new'
+get '/', to: 'hugs_and_coffes#new'
 #  resources :hugs_and_coffes, only: [:new],  as: :hugsAndCoffes
 resources :estado, :operation
 resources :perfils, except: [:index]
