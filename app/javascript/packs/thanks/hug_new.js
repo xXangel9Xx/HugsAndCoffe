f7e3be8599429f849af349933a77572c6ea41b5c
xXangelXx <angeluchimag17@gmail.com>
2020-07-12 12:19:23 -0400

    seconds ready

--- app/javascript/packs/thanks/hug_new.js
+++ app/javascript/packs/thanks/hug_new.js
@@ -2,20 +2,30 @@ export default function Hug(){
   this.context = function(){
       return document.querySelector("[data-context]").dataset['context'];
   };
+
+  this.animationHug = function(){
+    
+  }
+
+
+
 let contador = 0
   this.hugSeconds = function(){
     let circleHug = document.getElementsByClassName("circle-hug")[0];
+    let seconds = document.getElementsByClassName("seconds")[0];
     (function(){
       let interval
       circleHug.addEventListener('mouseup',(e)=>{
-          console.log(` CREO QUE LO LOGRE ${contador}`)
+          console.log(`${contador}`)
           clearInterval(interval)
       })
 
     circleHug.addEventListener('mousedown', (e)=>{
       let contar = function(){
         contador++
-        console.log(`${contador}`)
+        if (contador<60){
+        seconds.innerHTML = contador+'S'
+         }
       }
      interval =  setInterval(contar,1000)
     },true)
