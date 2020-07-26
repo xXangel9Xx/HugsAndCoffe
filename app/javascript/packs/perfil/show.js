10c3bbafad6ea0ebe5844c136a60c59645b32c49
xXangelXx <angeluchimag17@gmail.com>
2020-07-26 14:33:58 -0400

    i need fix pass image gon

--- app/javascript/packs/perfil/show.js
+++ app/javascript/packs/perfil/show.js
@@ -6,7 +6,7 @@ export default function Show(){
         let imageShow = document.getElementsByClassName('container-image-show')[0];
         let states = gon.states
        /* let images = gon.galeries
-        console.log(images)*/
+        console.log(images) < esto no funciona*/
         console.log(states)
            imageShow.innerHTML = `${states[0].record.subtitulo}`
        
